a0650f587344865486dce4cd8223fdbdf752a86f
Matthew Andrews <matt@mattandre.ws>
2014-08-25 18:04:06 +0100

    Support local development with --local argument

--- index.js
+++ index.js
@@ -3,14 +3,15 @@ var cors = require('cors');
 var stories = require('./lib/stories');
 var rssSource = require('./lib/rss-source');
 var port = Number(process.env.PORT || 3000);
+var feedUrl = process.argv.indexOf('--local') !== -1
+  ? 'http://localhost:8080/err-sample-rss.xml'
+  : 'http://feeds2.feedburner.com/ft/tech-blog';
 
 express()
   .use(cors())
   .get('/', function(req, res) {
     res.redirect(302, 'https://github.com/matthew-andrews/offline-news-api');
   })
-  .use('/stories', stories(rssSource({
-    url: 'http://feeds2.feedburner.com/ft/tech-blog'
-  })))
+  .use('/stories', stories(rssSource({ url: feedUrl })))
   .listen(port);
 console.log('listening on '+port);
